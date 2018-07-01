d34f69ef2dc0a32b6d177b436fcba827f1d8a218
Farkas Bence Gabor <bencegaborfarkas@gmail.com>
2018-07-01 19:16:33 +0200

    One page application with minimal css

---
+++ client/src/components/App/App.js
@@ -0,0 +1,130 @@
+import React from "react";
+import "./App.css";
+import Client from "../../Client";
+
+const MATCHING_ITEM_LIMIT = 10;
+
+class App extends React.Component {
+
+    state: {
+        foods: [],
+        searchValue: ""
+    };
+
+    componentWillMount() {
+        // Init state
+        this.setState({
+            foods: [],
+            searchValue: ""
+        });
+    };
+
+    componentDidMount() {
+        // Fetch data
+        Client.search("cheese", foods => {
+            this.setState({
+                foods: foods.slice(0, MATCHING_ITEM_LIMIT),
+                searchValue: "cheese"
+            });
+        });
+    };
+
+    search = (e) => {
+
+        const value = e.target.value;
+
+        this.setState({
+            searchValue: value
+        });
+
+        if (value === "") {
+            this.setState({
+                foods: [],
+                searchValue: ""
+            });
+        }
+        else {
+            // Fetch data
+            Client.search(value, foods => {
+                this.setState({
+                    foods: foods.slice(0, MATCHING_ITEM_LIMIT)
+                });
+            });
+        }
+
+    };
+
+    render() {
+
+        let foodRows;
+        let searchValue;
+
+        if(this.state){
+
+            foodRows = this.state.foods.map((food, idx) => (
+                <tr key={idx}>
+                    <td>{food.description}</td>
+                    <td className="right aligned">{food.kcal}</td>
+                    <td className="right aligned">{food.protein_g}</td>
+                    <td className="right aligned">{food.fat_g}</td>
+                    <td className="right aligned">{food.carbohydrate_g}</td>
+                </tr>
+            ));
+
+            searchValue = this.state.searchValue;
+        }
+
+        let html =
+            <div className="App">
+                <div className="Title">
+                    <h1>Food Lookup Demo Application</h1>
+                </div>
+                <div className="Content">
+                    <div className="Search">
+                        <form action="">
+                            <div>
+                                <label htmlFor="Search" id="SearchLabel">Search: </label>
+                                <input type="text" name="Search" id="Search" value={searchValue} onChange={this.search}/>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="Table">
+                        <table>
+                            <thead className="THead">
+                            <tr>
+                                <th>Description</th>
+                                <th>Carbohydrate (g)</th>
+                                <th>Fat (g)</th>
+                                <th>Kcal</th>
+                                <th>Protein (g)</th>
+                            </tr>
+                            </thead>
+                            <tbody className="TBody">
+                            {foodRows}
+                            </tbody>
+                            <tfoot className="TFoot">
+                            <tr>
+                                <td colSpan="4"><small>(This table contains nutrition facts about the searched items)</small></td>
+                            </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+                <div className="Footer">
+                    <footer>
+                        <p>
+                            <small><i>Author: Farkas Bence Gábor</i></small>
+                        </p>
+                        <p>
+                            <small><i>Date: 2018</i></small>
+                        </p>
+                    </footer>
+                </div>
+            </div>
+        ;
+
+        return (html);
+    }
+}
+
+export default App;
--- client/src_old/index.js
+++ client/src_old/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App/App";
+import App from "./App";
 import "./index.css";
+import "./semantic/dist/semantic.min.css";
 
 ReactDOM.render(
   <App />,
